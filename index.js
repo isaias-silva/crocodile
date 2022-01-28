fca30971cc1864abd7026c0fdda5889b9287baba
isaias-silva <zackblackmangold@gmail.com>
2022-01-27 22:33:44 -0300

    nome

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const os=require('os')
-console.log(os)
+const { default: axios } = require('axios')
+const readline = require('readline-sync')
+const ascii = require('./src/ascii')
+
+console.log(ascii.logo())
+console.log(ascii.menu())
+function main() {
+    let comand = readline.question('comando: ')
+    switch (comand) {
+        case '1':
+            axios.get('http://gerador-nomes.herokuapp.com/nome/aleatorio').then((x) => {
+                console.log(`${x.data[0]} ${x.data[1]}`)
+                main()
+            })
+
+            break;
+        case '0':
+            console.log("bye")
+    }
+}
+
+main()
