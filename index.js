4080da6d8a3d4083075748c9594518f0139a71a0
isaias-silva <zackblackmangold@gmail.com>
2022-01-31 15:23:51 -0300

    ajust menu

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const readline = require('readline-sync')
 const ascii = require('./src/ascii')
 const opn=require("opn")
 console.log(ascii.logo())
-console.log(ascii.menu())
+
 async function main() {
+    console.clear()
+    console.log(ascii.menu())
     let comand = readline.question('comando: ')
     switch (comand) {
         case '1':
