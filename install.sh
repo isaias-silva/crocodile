6a982071fd2386a8a3e5caa95188df5fba377daf
isaias-silva <zackblackmangold@gmail.com>
2022-02-27 14:02:11 -0300

    otimizando

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 clear
 echo -e "\033[1;32m É O ZACK NÉ PAI"
 apt-get update && apt-get upgrade
-
+apt-get install node
+npm install axios -g 
+npm install readline-sync -g 
+npm install opn -g 
+npm install ddos-stress  -g 
+clear
 echo -e "\033[1;32m obrigado por instalar!!!"
 npm start
 
